6b7263b7494f576fd2d46df6abeab9f67ff14ae2
stevenkhouri <steven.j.khouri@gmail.com>
2022-11-16 17:00:26 -0600

    hello world

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.7.10"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    mainClass.set("localrps.AppKt")
+}
---
+++ src/main/kotlin/localrps/app.kt
@@ -0,0 +1,23 @@
+package localrps
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+    }.start(wait = true)
+}
+
+
+fun Application.configureRouting() {
+    routing {
+        get("/"){
+            call.respondText("Hello World!")
+        }
+    }
+}
